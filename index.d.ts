9dd2cfadf03469324951b9b22683da99d97f1152
Igor Savin <iselwin@gmail.com>
2021-09-08 22:58:19 +0300

    Add handler override for fastify funky

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,31 @@
 import { FastifyPluginCallback } from 'fastify'
+import {
+  ContextConfigDefault,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerBase,
+  RawServerDefault,
+} from 'fastify/types/utils'
+import { FastifyInstance } from 'fastify/types/instance'
+import { FastifyRequest } from 'fastify/types/request'
+import { FastifyReply } from 'fastify/types/reply'
+import { RouteGenericInterface } from 'fastify/types/route'
+
+declare module 'fastify' {
+  interface RouteHandlerMethod<
+    RawServer extends RawServerBase = RawServerDefault,
+    RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+    RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+    RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+    ContextConfig = ContextConfigDefault
+  > {
+    (
+      this: FastifyInstance<RawServer, RawRequest, RawReply>,
+      request: FastifyRequest<RouteGeneric, RawServer, RawRequest>,
+      reply: FastifyReply<RawServer, RawRequest, RawReply, RouteGeneric, ContextConfig>
+    ): Record<string, any> | (() => any)
+  }
+}
 
 declare const fastifyFunky: FastifyPluginCallback
 
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,30 +1,38 @@
-import { FastifyInstance, FastifyPluginCallback } from "fastify";
-import fastify from "fastify";
-import { expectType } from "tsd";
+import { FastifyInstance, FastifyPluginCallback, FastifyReply, FastifyRequest } from 'fastify'
+import fastify from 'fastify'
+import { expectType } from 'tsd'
 
-import { fastifyFunky as fastifyFunkyNamed } from "./";
-import fastifyFunkyDefault from "./";
-import * as fastifyFunkyStar from "./";
-import fastifyFunkyCjsImport = require("./");
-const fastifyFunkyCjs = require("./");
+import { fastifyFunky as fastifyFunkyNamed } from './'
+import fastifyFunkyDefault from './'
+import * as fastifyFunkyStar from './'
+import fastifyFunkyCjsImport = require('./')
+const fastifyFunkyCjs = require('./')
 
-const app: FastifyInstance = fastify();
-app.register(fastifyFunkyNamed);
-app.register(fastifyFunkyDefault);
-app.register(fastifyFunkyCjs);
-app.register(fastifyFunkyCjsImport.default);
-app.register(fastifyFunkyCjsImport.fastifyFunky);
-app.register(fastifyFunkyStar.default);
-app.register(fastifyFunkyStar.fastifyFunky);
+const app: FastifyInstance = fastify()
+app.register(fastifyFunkyNamed)
+app.register(fastifyFunkyDefault)
+app.register(fastifyFunkyCjs)
+app.register(fastifyFunkyCjsImport.default)
+app.register(fastifyFunkyCjsImport.fastifyFunky)
+app.register(fastifyFunkyStar.default)
+app.register(fastifyFunkyStar.fastifyFunky)
 
-expectType<FastifyPluginCallback>(fastifyFunkyNamed);
-expectType<FastifyPluginCallback>(fastifyFunkyDefault);
-expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.default);
-expectType<FastifyPluginCallback>(
-  fastifyFunkyCjsImport.fastifyFunky
-);
-expectType<FastifyPluginCallback>(fastifyFunkyStar.default);
-expectType<FastifyPluginCallback>(
-  fastifyFunkyStar.fastifyFunky
-);
-expectType<any>(fastifyFunkyCjs);
+expectType<FastifyPluginCallback>(fastifyFunkyNamed)
+expectType<FastifyPluginCallback>(fastifyFunkyDefault)
+expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.default)
+expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.fastifyFunky)
+expectType<FastifyPluginCallback>(fastifyFunkyStar.default)
+expectType<FastifyPluginCallback>(fastifyFunkyStar.fastifyFunky)
+expectType<any>(fastifyFunkyCjs)
+
+app.register(fastifyFunkyDefault)
+// this gives a type error:
+app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
+  return { right: { id: 1 } }
+})
+
+app.get('/func', (req: FastifyRequest, reply: FastifyReply) => {
+  return () => {
+    return { right: { id: 1 } }
+  }
+})
