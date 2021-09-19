2602abc3cf0fd6f601c5bf2993e08c2b597b0b51
Igor Savin <iselwin@gmail.com>
2021-09-20 00:52:49 +0300

    Add handler override for fastify funky (#50)

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,56 @@
 import { FastifyPluginCallback } from 'fastify'
+import {
+  ContextConfigDefault,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerBase,
+  RawServerDefault,
+} from 'fastify/types/utils'
+import { FastifyInstance } from 'fastify/types/instance'
+import { FastifyRequest } from 'fastify/types/request'
+import { FastifyReply } from 'fastify/types/reply'
+import { RouteGenericInterface } from 'fastify/types/route'
+
+declare module 'fastify' {
+  interface RouteHandlerMethod<
+    RawServer extends RawServerBase = RawServerDefault,
+    RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+    RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
+    RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+    ContextConfig = ContextConfigDefault
+  > {
+    (
+      this: FastifyInstance<RawServer, RawRequest, RawReply>,
+      request: FastifyRequest<RouteGeneric, RawServer, RawRequest>,
+      reply: FastifyReply<RawServer, RawRequest, RawReply, RouteGeneric, ContextConfig>
+    ): void | Promise<RouteGeneric['Reply'] | void> | FunkyReply<RouteGeneric['Reply']>
+  }
+}
 
 declare const fastifyFunky: FastifyPluginCallback
+declare type FunkyReply<T> =
+  | TaskEither<unknown, T>
+  | Either<unknown, T>
+  | Right<T>
+  | Left
+  | Task<T>
+  | (() => FunkyReply<T>)
+
+declare interface Left<E = unknown> {
+  readonly left: E
+}
+
+declare interface Right<A> {
+  readonly right: A
+}
+
+declare type Either<E, A> = Left<E> | Right<A>
+
+declare interface Task<A> {
+  (): Promise<A>
+}
+
+declare interface TaskEither<E, A> extends Task<Either<E, A>> {}
 
 export default fastifyFunky
-export { fastifyFunky }
+export { fastifyFunky, FunkyReply, Left, Right, Either, Task, TaskEither }
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,30 +1,80 @@
-import { FastifyInstance, FastifyPluginCallback } from "fastify";
-import fastify from "fastify";
-import { expectType } from "tsd";
+import { FastifyInstance, FastifyPluginCallback, FastifyReply, FastifyRequest } from 'fastify'
+import fastify from 'fastify'
+import { either, task, taskEither } from 'fp-ts'
+import { expectType, expectError } from 'tsd'
 
-import { fastifyFunky as fastifyFunkyNamed } from "./";
-import fastifyFunkyDefault from "./";
-import * as fastifyFunkyStar from "./";
-import fastifyFunkyCjsImport = require("./");
-const fastifyFunkyCjs = require("./");
+import { fastifyFunky as fastifyFunkyNamed } from './'
+import fastifyFunkyDefault from './'
+import * as fastifyFunkyStar from './'
+import fastifyFunkyCjsImport = require('./')
+const fastifyFunkyCjs = require('./')
 
-const app: FastifyInstance = fastify();
-app.register(fastifyFunkyNamed);
-app.register(fastifyFunkyDefault);
-app.register(fastifyFunkyCjs);
-app.register(fastifyFunkyCjsImport.default);
-app.register(fastifyFunkyCjsImport.fastifyFunky);
-app.register(fastifyFunkyStar.default);
-app.register(fastifyFunkyStar.fastifyFunky);
+const app: FastifyInstance = fastify()
+app.register(fastifyFunkyNamed)
+app.register(fastifyFunkyDefault)
+app.register(fastifyFunkyCjs)
+app.register(fastifyFunkyCjsImport.default)
+app.register(fastifyFunkyCjsImport.fastifyFunky)
+app.register(fastifyFunkyStar.default)
+app.register(fastifyFunkyStar.fastifyFunky)
 
-expectType<FastifyPluginCallback>(fastifyFunkyNamed);
-expectType<FastifyPluginCallback>(fastifyFunkyDefault);
-expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.default);
-expectType<FastifyPluginCallback>(
-  fastifyFunkyCjsImport.fastifyFunky
-);
-expectType<FastifyPluginCallback>(fastifyFunkyStar.default);
-expectType<FastifyPluginCallback>(
-  fastifyFunkyStar.fastifyFunky
-);
-expectType<any>(fastifyFunkyCjs);
+expectType<FastifyPluginCallback>(fastifyFunkyNamed)
+expectType<FastifyPluginCallback>(fastifyFunkyDefault)
+expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.default)
+expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.fastifyFunky)
+expectType<FastifyPluginCallback>(fastifyFunkyStar.default)
+expectType<FastifyPluginCallback>(fastifyFunkyStar.fastifyFunky)
+expectType<any>(fastifyFunkyCjs)
+
+app.register(fastifyFunkyDefault)
+// this gives a type error:
+app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
+  return { right: { id: 1 } }
+})
+
+app.get('/func', (req: FastifyRequest, reply: FastifyReply) => {
+  return () => {
+    return { right: { id: 1 } }
+  }
+})
+
+app.get('/func', (req: FastifyRequest, reply: FastifyReply) => {
+  return Promise.resolve({})
+})
+
+app.get('/func', (req: FastifyRequest, reply: FastifyReply) => {
+  reply.status(200).send({})
+})
+
+// this is allowed
+app.get("/", (req, reply) => {
+  return { right: { id: 1 } };
+});
+app.get("/", (req, reply) => {
+  return { left: new Error('error') };
+});
+app.get("/", (req, reply) => {
+  return taskEither.fromEither(either.left(new Error('Invalid state')))
+});
+app.get("/", (req, reply) => {
+  return taskEither.fromTask(task.of(Promise.resolve({})))
+});
+app.get("/", (req, reply) => {
+  return either.of(Promise.resolve({}))
+});
+app.get("/", (req, reply) => {
+  return task.of(Promise.resolve({}))
+});
+app.get("/", (req, reply) => {
+  return taskEither.of(Promise.resolve({}))
+});
+
+//this will actually work in js, but not encouraged and hence ts will throw an error
+expectError(app.get("/", (req, reply) => {
+  return {}
+}))
+
+// ...but this gives a (correct) type error
+expectError(app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
+  return { rght: { id: 1 } }
+}))
