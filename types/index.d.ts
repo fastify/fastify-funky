1a9a0adbbc0fb2ced77928a2eb38418c654caf17
uzlopak <aras.abbasi@googlemail.com>
2023-12-26 06:01:14 +0100

    workaround: make types work with latest fastify

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,31 +1,4 @@
 import { FastifyPluginCallback } from 'fastify'
-import {
-  ContextConfigDefault,
-  RawReplyDefaultExpression,
-  RawRequestDefaultExpression,
-  RawServerBase,
-  RawServerDefault,
-} from 'fastify/types/utils'
-import { FastifyInstance } from 'fastify/types/instance'
-import { FastifyRequest } from 'fastify/types/request'
-import { FastifyReply } from 'fastify/types/reply'
-import { RouteGenericInterface } from 'fastify/types/route'
-
-declare module 'fastify' {
-  interface RouteHandlerMethod<
-    RawServer extends RawServerBase = RawServerDefault,
-    RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
-    RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
-    RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
-    ContextConfig = ContextConfigDefault
-  > {
-    (
-      this: FastifyInstance<RawServer, RawRequest, RawReply>,
-      request: FastifyRequest<RouteGeneric, RawServer, RawRequest>,
-      reply: FastifyReply<RawServer, RawRequest, RawReply, RouteGeneric, ContextConfig>
-    ): void | Promise<RouteGeneric['Reply'] | void> | fastifyFunky.FunkyReply<RouteGeneric['Reply']>
-  }
-}
 
 type FastifyFunky = FastifyPluginCallback
 
