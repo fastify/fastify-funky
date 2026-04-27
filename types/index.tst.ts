d9dbe3008ffa03e72c70b911d4e31c4074b8bf9d
Tony133 <a.tripodi133@gmail.com>
2026-04-27 09:53:07 +0200

    refactor(types): migrate from tsd to tstyche

--- types/index.tst.ts
+++ types/index.tst.ts
@@ -1,11 +1,11 @@
 import fastify, { FastifyInstance, FastifyPluginCallback, FastifyReply, FastifyRequest } from 'fastify'
 import { either, task, taskEither } from 'fp-ts'
-import { expectType, expectAssignable } from 'tsd'
-
+import { expect } from 'tstyche'
 import fastifyFunkyDefault, { fastifyFunky as fastifyFunkyNamed } from '..'
 import * as fastifyFunkyStar from '..'
-import fastifyFunkyCjsImport = require('..')
+
 const fastifyFunkyCjs = require('./')
+import fastifyFunkyCjsImport = require('..')
 
 const app: FastifyInstance = fastify()
 app.register(fastifyFunkyNamed)
@@ -16,13 +16,12 @@ app.register(fastifyFunkyCjsImport.fastifyFunky)
 app.register(fastifyFunkyStar.default)
 app.register(fastifyFunkyStar.fastifyFunky)
 
-expectType<FastifyPluginCallback>(fastifyFunkyNamed)
-expectType<FastifyPluginCallback>(fastifyFunkyDefault)
-expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.default)
-expectType<FastifyPluginCallback>(fastifyFunkyCjsImport.fastifyFunky)
-expectType<FastifyPluginCallback>(fastifyFunkyStar.default)
-expectType<FastifyPluginCallback>(fastifyFunkyStar.fastifyFunky)
-expectType<any>(fastifyFunkyCjs)
+expect(fastifyFunkyNamed).type.toBe<FastifyPluginCallback>()
+expect(fastifyFunkyDefault).type.toBe<FastifyPluginCallback>()
+expect(fastifyFunkyCjsImport.default).type.toBe<FastifyPluginCallback>()
+expect(fastifyFunkyCjsImport.fastifyFunky).type.toBe<FastifyPluginCallback>()
+expect(fastifyFunkyStar.default).type.toBe<FastifyPluginCallback>()
+expect(fastifyFunkyStar.fastifyFunky).type.toBe<FastifyPluginCallback>()
 
 app.register(fastifyFunkyDefault)
 // this gives a type error:
@@ -31,58 +30,64 @@ app.get('/', (_req: FastifyRequest, _reply: FastifyReply) => {
 })
 
 app.get('/func', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return () => {
     return { right: { id: 1 } }
   }
 })
 
 app.get('/func', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return Promise.resolve({})
 })
 
 app.get('/func', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   reply.status(200).send({})
 })
 
 // this is allowed
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return { right: { id: 1 } }
 })
+
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return { left: new Error('error') }
 })
+
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return taskEither.fromEither(either.left(new Error('Invalid state')))
 })
+
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return taskEither.fromTask(task.of(Promise.resolve({})))
 })
+
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return either.of(Promise.resolve({}))
 })
+
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return task.of(Promise.resolve({}))
 })
+
 app.get('/', (req, reply) => {
-  expectAssignable<FastifyRequest>(req)
-  expectAssignable<FastifyReply>(reply)
+  expect(req).type.toBeAssignableTo<FastifyRequest>()
+  expect(reply).type.toBeAssignableTo<FastifyReply>()
   return taskEither.of(Promise.resolve({}))
 })
